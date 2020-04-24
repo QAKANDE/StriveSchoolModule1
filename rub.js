ed873c6713d1a21c44bea5b7b5540e364b727a4c
QAKANDE <58850207+QAKANDE@users.noreply.github.com>
2020-04-24 07:55:55 +0100

    strivefiles

--- rub.js
+++ rub.js
@@ -1,5 +1,8 @@
-
-const qstring ="efretgfh,wugedhs,wjbksdsknc"
-const qarray = qstring.split("");
-
-console.log(qarray)
+let n = []
+//n.length = 10;
+for (let i=0 ; i<10 ; i++)
+{   
+    let n = [i]
+    let s = n.find(2)
+    console.log(s)
+}
